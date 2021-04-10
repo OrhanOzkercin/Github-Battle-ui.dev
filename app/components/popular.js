7e9ae981d62e29f23ec3054afc8d35ecf0e0ae12
Orhan Özkerçin <orhanozkercin@gmail.com>
2021-04-10 22:22:41 +0300

    index page and popular component update

---
+++ app/components/popular.js
@@ -0,0 +1,47 @@
+import * as React from 'react';
+
+function LangaugesNav({ selected, onUpdateLanguage }) {
+  const languages = ['All', 'JavaScript', 'Ruby', 'Java', 'CSS', 'Python'];
+
+  return (
+    <ul className='flex-center'>
+      {languages.map((language) => (
+        <li key={language}>
+          <button
+            className='btn-clear nav-link'
+            style={language === selected ? { color: 'rgb(187, 46, 31)' } : null}
+            onClick={() => onUpdateLanguage(language)}
+          >
+            {language}
+          </button>
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+export default class Popular extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      selectedLanguage: 'All',
+    };
+
+    this.updateLanguage = this.updateLanguage.bind(this);
+  }
+  updateLanguage(selectedLanguage) {
+    this.setState({
+      selectedLanguage,
+    });
+  }
+  render() {
+    const { selectedLanguage } = this.state;
+
+    return (
+      <React.Fragment>
+        <LangaugesNav selected={selectedLanguage} onUpdateLanguage={this.updateLanguage} />
+      </React.Fragment>
+    );
+  }
+}
--- app/index.js
+++ app/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import ReactDom from 'react-dom';
+import Popular from './components/popular';
 import './index.css';
 
 class App extends React.Component {
   render() {
-    return <h1>Hello Orhan</h1>;
+    return (
+      <div className='container'>
+        <Popular />
+      </div>
+    );
   }
 }
 
