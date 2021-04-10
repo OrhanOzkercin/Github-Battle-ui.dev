7dddf948a631a9658661ca8159ea8c6c8fdb1d70
Orhan Özkerçin <orhanozkercin@gmail.com>
2021-04-10 23:26:02 +0300

    Fetch data and cache it in popular component

--- app/components/popular.js
+++ app/components/popular.js
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import { fetchPopularRepos } from '../utils/api';
 
 function LangaugesNav({ selected, onUpdateLanguage }) {
   const languages = ['All', 'JavaScript', 'Ruby', 'Java', 'CSS', 'Python'];
@@ -26,21 +27,49 @@ export default class Popular extends React.Component {
 
     this.state = {
       selectedLanguage: 'All',
+      repos: {},
+      error: null,
     };
 
     this.updateLanguage = this.updateLanguage.bind(this);
   }
+
+  componentDidMount() {
+    this.updateLanguage('All');
+  }
+
   updateLanguage(selectedLanguage) {
     this.setState({
       selectedLanguage,
+
+      error: null,
     });
+
+    if (!this.state.repos[selectedLanguage]) {
+      fetchPopularRepos(selectedLanguage)
+        .then((data) => {
+          this.setState(({ repos }) => ({
+            repos: { ...repos, [selectedLanguage]: data },
+          }));
+        })
+        .catch((err) => this.setState({ error: err.message }));
+    }
   }
+
+  isLoading() {
+    const { repos, selectedLanguage, error } = this.state;
+    return !repos[selectedLanguage] && error === null;
+  }
+
   render() {
-    const { selectedLanguage } = this.state;
+    const { selectedLanguage, repos, error } = this.state;
 
     return (
       <React.Fragment>
         <LangaugesNav selected={selectedLanguage} onUpdateLanguage={this.updateLanguage} />
+        {this.isLoading() && <p>LOADING</p>}
+        {error && <p>{error}</p>}
+        {repos[selectedLanguage] && <pre>{JSON.stringify(repos[selectedLanguage], null, 2)}</pre>}
       </React.Fragment>
     );
   }
