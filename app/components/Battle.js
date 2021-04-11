4adb7fa4b9c6993f694e7b42a225aa45379cb0be
Orhan Özkerçin <orhanozkercin@gmail.com>
2021-04-11 10:25:16 +0300

    Result component create and implement in Battle component

--- app/components/Battle.js
+++ app/components/Battle.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { FaUserFriends, FaFighterJet, FaTrophy } from 'react-icons/fa';
+import { FaUserFriends, FaFighterJet, FaTrophy, FaTimesCircle } from 'react-icons/fa';
+import Results from './Results';
 
 function Instructions() {
   return (
@@ -67,12 +68,36 @@ class PlayerInput extends React.Component {
   }
 }
 
+function PlayerPreview({ username, onReset, label }) {
+  return (
+    <div className='column player'>
+      <h3 className='player-label'>{label}</h3>
+      <div className='row bg-light'>
+        <div className='player-info'>
+          <img
+            className='avatar-small'
+            src={`https://github.com/${username}.png?size=200`}
+            alt={`Avatar for ${username}`}
+          />
+          <a href={`https://github.com/${username}`} className='link'>
+            {username}
+          </a>
+        </div>
+        <button className='btn-clear flex-center' onClick={onReset}>
+          <FaTimesCircle color='rgb(194, 57, 42)' size={26} />
+        </button>
+      </div>
+    </div>
+  );
+}
+
 export default class Battle extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { playerOne: null, playerTwo: null };
+    this.state = { playerOne: null, playerTwo: null, battle: false };
 
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleReset = this.handleReset.bind(this);
   }
 
   handleSubmit(id, player) {
@@ -81,21 +106,40 @@ export default class Battle extends React.Component {
     });
   }
 
+  handleReset(id) {
+    this.setState({
+      [id]: null,
+    });
+  }
+
   render() {
-    const { playerOne, playerTwo } = this.state;
+    const { playerOne, playerTwo, battle } = this.state;
+
+    if (battle === true) {
+      return <Results playerOne={playerOne} playerTwo={playerTwo} />;
+    }
     return (
       <>
         <Instructions />
         <div className='players-container'>
           <h1 className='center-text header-lg'>Players</h1>
           <div className='row space-around'>
-            {playerOne === null && (
-              <PlayerInput label='Player 1' onSubmit={(player) => this.handleSubmit('playerOne', player)} />
+            {playerOne === null ? (
+              <PlayerInput label='Player One' onSubmit={(player) => this.handleSubmit('playerOne', player)} />
+            ) : (
+              <PlayerPreview username={playerOne} label='Player One' onReset={() => this.handleReset('playerOne')} />
             )}
-            {playerTwo === null && (
-              <PlayerInput label='Player 1' onSubmit={(player) => this.handleSubmit('playerTwo', player)} />
+            {playerTwo === null ? (
+              <PlayerInput label='Player Two' onSubmit={(player) => this.handleSubmit('playerTwo', player)} />
+            ) : (
+              <PlayerPreview username={playerTwo} label='Player Two' onReset={() => this.handleReset('playerTwo')} />
             )}
           </div>
+          {playerOne && playerTwo && (
+            <button className='btn dark-btn btn-space' onClick={() => this.setState({ battle: true })}>
+              Battle
+            </button>
+          )}
         </div>
       </>
     );
