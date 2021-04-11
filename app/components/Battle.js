8eecdf5aacf9f8cc1aa9568440342f0d0a99c6df
Orhan Özkerçin <orhanozkercin@gmail.com>
2021-04-11 09:22:28 +0300

    Input field and style add to it

--- app/components/Battle.js
+++ app/components/Battle.js
@@ -23,6 +23,50 @@ function Instructions() {
   );
 }
 
+class PlayerInput extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { username: '' };
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+  handleChange(e) {
+    this.setState({
+      username: e.target.value,
+    });
+  }
+  handleSubmit(e) {
+    e.preventDefault();
+
+    this.props.onSubmit(this.state.username);
+  }
+
+  render() {
+    return (
+      <form className='column player' onSubmit={this.handleSubmit}>
+        <label htmlFor='username' className='player-label'>
+          {this.props.label}
+        </label>
+        <div className='row player-inputs'>
+          <input
+            type='text'
+            id='username'
+            className='input-light'
+            placeholder='github username'
+            autoComplete='off'
+            value={this.state.username}
+            onChange={this.handleChange}
+          />
+          <button className='btn dark-btn' type='submit' disabled={!this.state.username}>
+            Submit
+          </button>
+        </div>
+      </form>
+    );
+  }
+}
+
 export default class Battle extends React.Component {
   render() {
     return (
